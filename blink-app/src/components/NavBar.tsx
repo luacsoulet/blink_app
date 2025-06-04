c15571bfd96b7d0a64b9833388e1e111dc50c3d7
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-06-04 18:29:54 +0200

    Refactor NavBar styles for clarity and reuse
    Moved scoped styles for .navbar, .navbar__logo, and .navbar__title to SCSS to reduce class name clutter in the component.
    Enhanced readability and applied consistent hover/active effects for logo and buttons.

--- blink-app/src/components/NavBar.tsx
+++ blink-app/src/components/NavBar.tsx
@@ -2,6 +2,7 @@
 import Link from "next/link"
 import { useAuthStore } from "@/store/AuthStore"
 import { CircleUserRound } from "lucide-react";
+import "@/styles/navBar.scss";
 
 export const NavBar = () => {
 
@@ -10,22 +11,22 @@ export const NavBar = () => {
         <div className="flex justify-between items-center p-5">
             <div>
                 <Link href="/">
-                    <h1 className="text-2xl font-bold hover:cursor-pointer hover:scale-105 active:scale-95 transition-all duration-300">Blink</h1>
+                    <h1 className="navbar__logo font-bold">Blink</h1>
                 </Link>
             </div>
             <div>
                 {
                     isAuthenticated ? (
                         <div className="flex items-center gap-4">
-                            <Link href="/profile" className="flex items-center gap-2 bg-secondary text-primary py-2 px-4 rounded-xl hover:cursor-pointer hover:bg-action/80 hover:text-secondary hover:scale-105 active:scale-95 transition-all duration-300">
+                            <Link href="/profile" className="navbar__title user">
                                 <span>{user?.username}</span>
                                 <CircleUserRound />
                             </Link>
-                            <button onClick={() => logout()} className="btn border btn-ghost border-secondary py-2 px-4 rounded-xl hover:cursor-pointer hover:bg-action/80 hover:border-action hover:scale-105 active:scale-95 transition-all duration-300">Logout</button>
+                            <button onClick={() => logout()} className="navbar__title auth">Logout</button>
                         </div>
                     ) : (
                         <div className="flex items-center gap-4">
-                            <Link href="/auth" className="btn border btn-ghost border-secondary py-2 px-4 rounded-xl hover:cursor-pointer hover:bg-action/80 hover:border-action hover:scale-105 active:scale-95 transition-all duration-300">
+                            <Link href="/auth" className="navbar__title auth">
                                 Login
                             </Link>
                         </div>
