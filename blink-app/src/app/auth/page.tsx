4febefcbb713110f9223b932741544145ae133fc
MAGALHAES Lucas <lucasmagalhaes45200@gmail.com>
2025-06-07 19:45:25 +0200

    Create Auth page and import Auth component

---
+++ blink-app/src/app/auth/page.tsx
@@ -0,0 +1,13 @@
+
+import { Auth } from '@/components/Auth';
+
+const AuthPage = () => {
+
+    return (
+        <div className="flex flex-col items-center justify-center h-screen">
+            <Auth />
+        </div>
+    );
+};
+
+export default AuthPage;
